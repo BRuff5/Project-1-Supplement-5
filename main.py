7fa04ffe4ad3600f1d14516eba6611b6de4dc0cf
BLR <bruff@Macky-Max.local>
2024-11-24 22:00:19 -0500

    this feature Return the square root of a number or raise an exceptionif negative

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import random
 import math
 
+def square_root(num):
+    if num < 0:
+        raise ValueError("INVALID NUMBER: Cannot calculate the square root of a negative number.")
+    return math.sqrt(num)
+
+    """Return the square root of a number or raise an exception if negative
+    Args:
+        Returns number
+    Return:
+        Squeres a number and if number is negative invalid response message appears
+    """
+    
