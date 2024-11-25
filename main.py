a12d6a55beab4cb287ae5321f2de8349a7abbba4
BLR <bruff@Macky-Max.local>
2024-11-24 22:49:29 -0500

    this feature sees if the inputted number is divisible 3 and if so divides that number by 3

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def process_random_integer():
         Displays random number that was generated
     """
     
-    if number % 2 != 0:  # Odd
+    if number % 2 != 0:  
         number *= 2
         print(f"Number is odd, multiplying by 2: {number}")
         
@@ -35,3 +35,14 @@ def process_random_integer():
         If user input is odd the it will be multiplyed by 2
     """
     
+    if number % 3 == 0:  # Divisible by 3
+        number //= 3
+        print(f"Number is divisible by 3, dividing by 3: {number}")
+        
+    """If the number is divisible 3 divide it by 3
+    Args:
+        Recives user input
+    Result:
+        If user input is divisible 3 then divide number by 3
+    """
+        
